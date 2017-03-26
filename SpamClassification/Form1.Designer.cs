7045c4ec06db8be14f2a080b60109c5cf9e36515
aidbal <balcaitis.aidas@gmail.com>
2017-03-27 02:08:00 +0300

    Add project files.

---
+++ SpamClassification/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SpamClassification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(685, 422);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(704, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Pasirinkti spamo failus";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(793, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pasirinkti ne spamo failus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 447);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Spam Classification";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SpamClassification/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpamClassification
+
+{
+    public partial class Form1 : Form
+    {
+        private Data table;
+
+        private class Data
+        {
+            //int[0] žodžio pasikartojimas spamo laiškuose
+            //int[1] žodžio pasikartojimas ne spamo laiškuose
+            //int[2] žodžio tikimybė, kad jis spam
+            //int[3] žodžio tikimybė, kad jis ne spam
+            private Dictionary<string, int[]> table;
+            public int SpamCount { set; get; }
+            public int HamCount { set; get; }
+
+            public Data()
+            {
+                table = new Dictionary<string, int[]> { };
+                SpamCount = 0;
+                HamCount = 0;
+            }
+
+            public void addSpamPair(string word)
+            {
+                if (table.ContainsKey(word))
+                    table[word][0] += 1;
+                else
+                {
+                    int[] arr = new int[4];
+                    arr[0] = 1;
+                    arr[1] = 0;
+                    arr[2] = 0;
+                    arr[3] = 0;
+                    table.Add(word, arr);
+                }
+            }
+            public void addNotSpamPair(string word)
+            {
+                if (table.ContainsKey(word))
+                    table[word][1] += 1;
+                else
+                {
+                    int[] arr = new int[4];
+                    arr[0] = 0;
+                    arr[1] = 1;
+                    arr[2] = 0;
+                    arr[3] = 0;
+                    table.Add(word, arr);
+                }
+            }
+            public int[] getValue(string word)
+            {
+                return table[word];
+            }
+            public Dictionary<string, int[]> getAllValues()
+            {
+                return table;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            table = new Data();
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog fop = new OpenFileDialog();
+                fop.Multiselect = true;
+                fop.InitialDirectory = "D:\\STUDIJOS\\6 semestras\\Intelektikos pagrindai\\L3";
+                fop.Filter = "txt|*.txt";
+                fop.Title = "Pasirinkti spamo failus";
+
+                String pattern = @"[A-Za-z0-9$'""]+";
+
+                if (fop.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (String filename in fop.FileNames)
+                    {
+                        FileStream FS = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                        var lines = File.ReadLines(filename);
+                        foreach (var line in lines)
+                        {
+                            foreach (Match m in Regex.Matches(line, pattern))
+                            {
+                                table.addSpamPair(m.Value);
+                            }
+                        }
+                        table.SpamCount += 1;
+                        FS.Close();
+                    }
+                    MessageBox.Show("Spamo failai sėkmingai nuskaityti!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                foreach (KeyValuePair<string, int[]> pair in table.getAllValues())
+                {
+                    richTextBox1.Text += pair.Key + ": " + pair.Value[0] + ", " + pair.Value[1] + "\n";
+                    //Console.WriteLine("Key: {0} Values: {1},{2}",
+                    //    pair.Key,
+                    //    pair.Value[0], pair.Value[1]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog fop = new OpenFileDialog();
+                fop.Multiselect = true;
+                fop.InitialDirectory = "D:\\STUDIJOS\\6 semestras\\Intelektikos pagrindai\\L3";
+                fop.Filter = "txt|*.txt";
+                fop.Title = "Pasirinkti ne spamo failus";
+
+                String pattern = @"[A-Za-z0-9$'""]+";
+
+                if (fop.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (String filename in fop.FileNames)
+                    {
+                        FileStream FS = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                        var lines = File.ReadLines(filename);
+                        foreach (var line in lines)
+                        {
+                            foreach (Match m in Regex.Matches(line, pattern))
+                            {
+                                table.addNotSpamPair(m.Value);
+                            }
+                        }
+                        table.HamCount += 1;
+                        FS.Close();
+                    }
+                    MessageBox.Show("Ne spamo failai sėkmingai nuskaityti!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
